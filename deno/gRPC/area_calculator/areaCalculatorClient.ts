83c62cd91a41f781175a3bf3e3bee7391d38d3b7
Yousaf Nabi <you@saf.dev>
2022-11-27 21:59:49 +0000

    deno: win paths

--- deno/gRPC/area_calculator/areaCalculatorClient.ts
+++ deno/gRPC/area_calculator/areaCalculatorClient.ts
@@ -6,9 +6,13 @@ console.log('area calc proto path')
 console.log(path.join(Deno.cwd(),'proto','area_calculator.proto'))
 let protoFile:string;
 
- const protoPath = new URL(path.join(Deno.cwd(),'proto','area_calculator.proto'), import.meta.url);
- console.log(Deno.build.os)
+const protoPath = new URL(path.join(Deno.cwd(),'proto','area_calculator.proto'), import.meta.url);
+console.log("protoPath",protoPath)
+console.log(Deno.build.os)
  if (Deno.build.os === "windows") {
+  console.log("path.join",path.join(Deno.cwd(),'proto','area_calculator.proto'))
+  console.log("protoPath",protoPath)
+  console.log("path.join",path.join(Deno.cwd(),'proto','area_calculator.proto'))
    protoFile = await Deno.readTextFile("D:\\a\\hello_ffi\\hello_ffi\\proto\\area_calculator.proto");
  } else {
   protoFile = await Deno.readTextFile(protoPath);
