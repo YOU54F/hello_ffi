909878100cb4bc429cd66dcacb970f5ee05ffd5b
Yousaf Nabi <you@saf.dev>
2022-11-27 22:02:19 +0000

    deno: win paths

--- deno/gRPC/area_calculator/areaCalculatorClient.ts
+++ deno/gRPC/area_calculator/areaCalculatorClient.ts
@@ -7,16 +7,16 @@ console.log(path.join(Deno.cwd(),'proto','area_calculator.proto'))
 let protoFile:string;
 
 const protoPath = new URL(path.join(Deno.cwd(),'proto','area_calculator.proto'), import.meta.url);
-console.log("protoPath",protoPath)
-console.log(Deno.build.os)
- if (Deno.build.os === "windows") {
-  console.log("path.join",path.join(Deno.cwd(),'proto','area_calculator.proto'))
-  console.log("protoPath",protoPath)
-  console.log("path.join",path.join(Deno.cwd(),'proto','area_calculator.proto'))
-   protoFile = await Deno.readTextFile("D:\\a\\hello_ffi\\hello_ffi\\proto\\area_calculator.proto");
- } else {
-  protoFile = await Deno.readTextFile(protoPath);
- }
+// console.log("protoPath",protoPath)
+// console.log(Deno.build.os)
+//  if (Deno.build.os === "windows") {
+//   console.log("path.join",path.join(Deno.cwd(),'proto','area_calculator.proto'))
+//   console.log("protoPath",protoPath)
+//   console.log("path.join",path.join(Deno.cwd(),'proto','area_calculator.proto'))
+   protoFile = await Deno.readTextFile(protoPath.href);
+//  } else {
+//   protoFile = await Deno.readTextFile(protoPath);
+//  }
 
 export async function getShapeMessage(port:number) {
   const client = getClient<Calculator>({
