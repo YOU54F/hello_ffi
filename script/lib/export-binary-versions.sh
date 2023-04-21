de07a7f4a32fb28deb840cd17d7ef151b67086b5
Yousaf Nabi <you@saf.dev>
2023-04-21 22:51:22 +0100

    chore: read ffi version from file

--- script/lib/export-binary-versions.sh
+++ script/lib/export-binary-versions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 LIB_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
 PROJECT_DIR="${LIB_DIR}"/../../
-export FFI_VERSION=v0.3.15
+export FFI_VERSION=v$(cat $PROJECT_DIR/FFI_VERSION)
 export PLUGIN_CLI_VERSION=v0.0.1
